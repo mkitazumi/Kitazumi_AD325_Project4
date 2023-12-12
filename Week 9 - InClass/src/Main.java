684bf0c2ac4e8e58a30cd141dc5cbf9cdaca6c5d
Ashtin <ashtinrivada@yahoo.com>
2023-12-11 18:27:24 -0800

    Worked on ProfileManager as best as I could so far

--- Week 9 - InClass/src/Main.java
+++ Week 9 - InClass/src/Main.java
@@ -1,5 +1,8 @@
 public class Main {
     public static void main(String[] args) {
-
+        Profile me = new Profile("Ashtin");
+        ProfileManager manager = new ProfileManager(me);
+        manager.addProfile(me);
+        System.out.println();
     }
 }
--- Week 9 - InClass/src/ProfileManager.java
+++ Week 9 - InClass/src/ProfileManager.java
@@ -2,6 +2,42 @@ import GraphPackage.UndirectedGraph;
 
 public class ProfileManager {
    private UndirectedGraph<Profile> network;
+   private Profile origin;
+
+   public ProfileManager(Profile origin){
+      network = new UndirectedGraph<>();
+      this.origin = origin;
+
+   }
+
+   /**
+    * Adds a Profile to the Graph
+    * @param profile profile wanting to be added
+    */
+   public void addProfile(Profile profile){
+      if (network.isEmpty()){
+         origin = profile;
+         network.addVertex(origin);
+
+      }
+      network.addVertex(profile);
+   }
+
+   public void connect(Profile vertex1, Profile vertex2){
+      network.addEdge(vertex1, vertex2);
+   }
+
+   public void Display(){
+   }
+
+   public void DisplayBFS(){
+
+   }
+
+   public Profile removeProfile(Profile profile){
+      Profile removedProfile = null;
+      return removedProfile;
+   }
 
 
 
